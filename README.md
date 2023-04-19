65a24cae8dc8678781a888f4c481721015df13e0
Jack Lu <jacklu@jacklu.me>
2023-04-19 10:02:44 +0800

    +WechatyChatscriptPlugin

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # wechaty-chatscript
 
 A Wechaty plugin that implements chatscript.
+
+## Usage
+
+```js
+var { Wechaty } = require('wechaty');
+var WechatyChatscriptPlugin = require('wechaty-chatscript');
+var bot = new Wechaty();
+bot.use(
+	WechatyChatscriptPlugin(`
+		on receive message do (
+			message.text = "ding" ?
+				(send "dong" to message.from;) : 0
+		);
+	`)
+);
+```
---
+++ index.js
@@ -0,0 +1,43 @@
+/** @typedef { import("wechaty").Wechaty } Wechaty */
+/** @typedef { import("wechaty").Room } Room */
+/** @typedef { import("wechaty").Message } Message */
+var chatscript = require('chatscript');
+var ORIGINAL = Symbol('original');
+
+/**
+ * @param {string} program
+ */
+module.exports = function WechatyChatscriptPlugin(program) {
+	return function (/** @type {Wechaty} */bot) {
+		var environment = new chatscript.Environment(new chatscript.Scope({}));
+		var machine = new chatscript.Machine(environment, {
+			async send(message, receiver) {
+				await receiver[ORIGINAL].say(message.value);
+			}
+		});
+		(async () => {
+			machine.run(chatscript.parse(program));
+			while (!(await machine.step()));
+			bot.on('message', async (/** @type {Message} */message) => {
+				var g = machine.emit({
+					type: 'receive',
+					argument: new Message(message)
+				});
+				while (!(await g.next()).done);
+			});
+		})();
+		/** @param {import("wechaty").Contact} contact */
+		function Contact(contact) {
+			return Object.assign(new chatscript.Value.Object({
+				get name() { return contact.name() }
+			}), { [ORIGINAL]: contact });
+		}
+		/** @param {import("wechaty").Message} message */
+		function Message(message) {
+			return Object.assign(new chatscript.Value.Object({
+				get text() { return new chatscript.Value.String(message.text()); },
+				get from() { return new Contact(message.talker()); }
+			}), { [ORIGINAL]: message });
+		}
+	};
+};
